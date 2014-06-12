48e88b45774947815275580f850dea899e01ca6c
sallylaw <sallylaw@users.noreply.github.com>
2014-06-12 01:07:31 -0700

    Create RX_2_transmit_pulses.ino
    For transmit Pulses Demonstration.

---
+++ RX_2_transmit_pulses.ino
@@ -0,0 +1,56 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN   9
+#define CSN_PIN 10
+#define analogInPin A0
+const int analogOutPin = 5;
+int outputValue = 0;
+int HzSample = 200000;
+int sample=0;
+
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+int sensorValue;
+
+void setup()   /****** SETUP: RUNS ONCE ******/
+{
+  Serial.begin(57600);
+  radio.begin();
+  radio.setRetries(15,15);
+  radio.openWritingPipe(pipes[0]);
+  radio.openReadingPipe(1,pipes[1]);
+  radio.startListening();
+  radio.printDetails();  
+  pinMode(analogOutPin, OUTPUT);
+}//--(end setup )---
+
+void loop(void)
+{    
+    if ( radio.available() )        // if there is data ready
+    {
+      unsigned long message=0;        // Dump the payloads until we've gotten everything
+      bool done = false;
+      while (!done)
+      {
+        done = radio.read( &message, sizeof(unsigned long) );  // Fetch the payload, and see if this was the last one.
+        Serial.print("message = ");
+        Serial.print(message);
+        Serial.print("\n");
+        
+        /************************/
+        //testing with A0 input, need to change value to message later
+        sample = map(message, 0, 4095, 0, HzSample);
+        analogWrite(6, sample);  // write the selected waveform on DAC0
+  
+     /*   Serial.write( 0xff );                                                         
+        Serial.write( (message >> 8) & 0xff );                                            
+        Serial.write( message & 0xff );*/
+        
+       // analogWrite(analogOutPin, message);
+
+   // delay(200);       // Delay just a little bit to let the other unit make the transition to receiver
+      }
+    }
+}
